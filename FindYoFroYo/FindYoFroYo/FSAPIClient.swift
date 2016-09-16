0305fb5c5bbfdc915fca101b537ec38c0a726b9a
ellscha <escharlin@wesleyan.edu>
2016-09-16 08:19:53 -0400

    findYoFroYo xcodeproject added and foursqaure api client added

---
+++ FindYoFroYo/FindYoFroYo/FSAPIClient.swift
@@ -0,0 +1,32 @@
+//
+//  FSAPIClient.swift
+//  FindYoFroYo
+//
+//  Created by Elli Scharlin on 9/16/16.
+//  Copyright © 2016 ElliScharlin. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+import CoreLocation
+
+struct FoursquareConstants {
+    static let v = "20160808"
+    static let query = "Frozen Yogurt"
+}
+
+
+class FoursquareAPIClient {
+    
+    class func getQueryForSearchLandmarks(parameter: [String: String], completion: (JSON?, ErrorType?) -> ()) {
+        
+        Alamofire.request(.GET, ATConstants.Endpoints.FOURSQUARE_GET_VENUES, parameters: parameter, headers: nil).responseJSON { response in
+            guard let data = response.data, responseJSON = JSON(data: data).dictionary?["response"] else {
+                completion(nil,response.result.error)
+                return
+            }
+            completion(responseJSON, nil)
+        }
+    }
+}
